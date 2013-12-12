a6f5daed284d875bbdd97acc94ecc8bb97eae8cf
Wil Helser <wil@wilhelser.com>
2013-12-12 15:07:30 -0600

    before galleries

--- app/admin/contribution.rb
+++ app/admin/contribution.rb
@@ -1,3 +1,46 @@
 ActiveAdmin.register Contribution do
+  # Filters
+  filter :project
+  filter :amount
+  filter :private
+  filter :first_name
+  filter :last_name
+  filter :address
+  filter :city
+  filter :state
+  filter :email
 
+  # Index
+  index do
+    selectable_column
+    column :id
+    column :project
+    column :private
+    column :first_name
+    column :last_name
+    default_actions
+  end
+
+  # Show
+  show do |contribution|
+    attributes_table do
+      row :id
+      row :project
+      row :amount
+      row :first_name
+      row :last_name
+      row :email
+      row "Type" do
+        contribution.project.categories.each do |c|
+          link_to "#{c.name}", admin_category_path(c)
+        end
+      end
+      row :address
+      row :address_2
+      row :city
+      row :state
+      row :zip_code
+      row :private
+    end
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,8 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :projects
   attr_accessible :name, :order, :description, :icon, :slug
   validates_uniqueness_of :slug
+
+  def to_s
+    self.name
+  end
 end
