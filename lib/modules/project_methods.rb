e31b9cba7617afafdcaef48ebd7f90d268f5739a
Wil Helser <wil@wilhelser.com>
2013-12-12 23:04:02 -0600

    add whenever schedule.rb

--- lib/modules/project_methods.rb
+++ lib/modules/project_methods.rb
@@ -2,4 +2,11 @@ module ProjectMethods
   def send_funded_email
 
   end
+
+
+  # Checks for inactive actions and sends an email
+  # called from config/schedule.rb
+  def email_inactive
+
+  end
 end
