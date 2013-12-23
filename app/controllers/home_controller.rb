2b2cc3941a063e02c0c34f3bb96eed5b5baa087e
Wil Helser <wil@wilhelser.com>
2013-12-23 10:43:09 -0600

    projects index limit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,14 @@ class HomeController < ApplicationController
 
   def index
     @page_title = "Changing the World Together"
-    @projects = Project.donatable.limit(4)
+    @projects = Project.donatable.last(4)
     @categories = Category.all
     @projects_near_me = Project.donatable.near([@lat, @long], 50).limit(4)
     if user_signed_in?
       @projects_from_friends = current_user.get_friends_projects
       @friends_projects = @projects_from_friends.first(4)
     end
-    @oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'],"#{ENV['FB_SECRET_KEY']}", "http://compassion.dev/home/fb_callback")
+    @oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'],"#{ENV['FB_SECRET_KEY']}", "http://compassionforhumanity.org/home/fb_callback")
     @oauth_url = @oauth.url_for_oauth_code
     unless session[:temp_token].blank?
       @token = session[:temp_token]
@@ -22,7 +22,7 @@ class HomeController < ApplicationController
   end
 
   def fb_callback
-    @oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'],"#{ENV['FB_SECRET_KEY']}", "http://compassion.dev/home/fb_callback")
+    @oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'],"#{ENV['FB_SECRET_KEY']}", "http://compassionforhumanity.org/home/fb_callback")
     if params[:code]
       @cookie = params[:code]
       @token = @oauth.get_access_token(@cookie)
