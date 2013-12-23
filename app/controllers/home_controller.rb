1ffd750fe030c671b6e9d6fadd01817ea5763a17
Wil Helser <wil@wilhelser.com>
2013-12-23 09:19:16 -0600

    add created by friends section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  include FriendsProjects
   layout Proc.new { |controller| controller.request.xhr?? false : 'application' }
   respond_to :html, :json, :js
 
@@ -9,6 +10,15 @@ class HomeController < ApplicationController
     @projects_near_me = Project.donatable.near([@lat, @long], 50).limit(4)
     if user_signed_in?
       @projects_from_friends = current_user.get_friends_projects
+      @friends_projects = @projects_from_friends.first(4)
+    end
+    @oauth = Koala::Facebook::OAuth.new(231408540317089,"7758a40a88cf75e51df02496c4390078", "http://compassionforhumanity.org")
+    @oauth_url = @oauth.url_for_oauth_code
+    if params[:code]
+      @cookie = params[:code]
+      @token = @oauth.get_access_token(@cookie)
+      session[:temp_token] = @token
+      @projects_from_friends = no_user_get_friends_projects(@token)
       @friends_projects = @projects_from_friends.first(4)
     end
   end
--- lib/modules/friends_projects.rb
+++ lib/modules/friends_projects.rb
@@ -1,7 +1,7 @@
 module FriendsProjects
 
   def get_friends_projects
-    @users =  get_my_friends
+    @users =  get_my_friends(self.token)
     @projects = []
     @users.each do |u|
       proj = u.projects
@@ -12,8 +12,21 @@ module FriendsProjects
     return @projects
   end
 
-  def get_my_friends
-    @graph = Koala::Facebook::API.new(self.token)
+  def no_user_get_friends_projects(token)
+    @token = token
+    @users =  get_my_friends(token)
+    @projects = []
+    @users.each do |u|
+      proj = u.projects
+      if proj.any?
+        proj.each {|p| @projects << p }
+      end
+    end
+    return @projects
+  end
+
+  def get_my_friends(token)
+    @graph = Koala::Facebook::API.new(token)
     @friends = @graph.get_connections('me', 'friends')
     @ids = []
     @friends.each {|f| @ids << f['id']}
