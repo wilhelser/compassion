085ab1f2886e46ea3ca42fabbe6c7f0d55ec57ba
Wil Helser <wil@wilhelser.com>
2013-12-30 16:53:49 -0600

    filter commas in amount

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,18 +2,14 @@ class Vendor < ActiveRecord::Base
   attr_accessible :address, :amount, :city, :contact_name, :description, :documentation, :invoice_number, :name, :phone, :project_id, :state, :verified, :zip_code, :due_date, :account_no, :paid
   belongs_to :project
   validates :amount, :name, :address, :city, :state, :zip_code, :phone, :presence => true
+  validates_numericality_of :amount, :message => "must not contain any punctuation"
   mount_uploader :documentation, VendorUploader
-  before_save :strip_commas
 
   scope :verified, -> { where(verified: true) }
   scope :paid, -> { where(paid: true) }
 
   def vendor_total
     project.vendors.sum(:amount)
-  end
-
-  def strip_commas
-    self.amount.gsub!(',', '')
   end
 
 end
