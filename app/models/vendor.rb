e5536e4ec62c78887b0d0cdca7952ce018ae8c5a
Wil Helser <wil@wilhelser.com>
2013-12-30 15:52:25 -0600

    require phone for vendors

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,7 +1,7 @@
 class Vendor < ActiveRecord::Base
   attr_accessible :address, :amount, :city, :contact_name, :description, :documentation, :invoice_number, :name, :phone, :project_id, :state, :verified, :zip_code, :due_date, :account_no, :paid
   belongs_to :project
-  validates :amount, :name, :address, :city, :state, :zip_code, :presence => true
+  validates :amount, :name, :address, :city, :state, :zip_code, :phone, :presence => true
   mount_uploader :documentation, VendorUploader
 
   scope :verified, -> { where(verified: true) }
