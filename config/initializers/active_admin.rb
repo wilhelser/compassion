1071c1acdd23e2ae40f932a4af95d5d4ab640a2a
Wil Helser <wil@wilhelser.com>
2013-12-31 14:37:58 -0600

    admin logo

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -17,7 +17,7 @@ ActiveAdmin.setup do |config|
   #
   # Note: Recommended image height is 21px to properly fit in the header
   #
-  config.site_title_image = "/assets/admin-logo.png"
+  config.site_title_image = "/admin-logo.png"
 
   # == Default Namespace
   #
