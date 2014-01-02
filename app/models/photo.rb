9553c42ff9406305a7e1c223073e91b28c7c1f65
Wil Helser <wil@wilhelser.com>
2014-01-02 11:55:43 -0600

    image methods

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo < ActiveRecord::Base
   include ImageMethods
   attr_accessible :caption, :gallery_id, :image, :key
+  validates_presence_of :image
   belongs_to :gallery, dependent: :destroy, touch: true
   after_create :set_key
 
