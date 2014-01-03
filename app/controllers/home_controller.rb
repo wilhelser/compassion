ed81b7a70654d7594b233256b591f77af21cdacc
Wil Helser <wil@wilhelser.com>
2014-01-02 19:59:13 -0600

    use request.remote_ip for geolocation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
   include FriendsProjects
   layout Proc.new { |controller| controller.request.xhr?? false : 'application' }
   respond_to :html, :json, :js
-  # before_filter :get_request_coordinates, only: [:index]
-
+  # before_filter :get_request_coordinates, only: [:index@import "☄";]
   def index
     @page_title = "Changing the World Together"
     @categories = Category.all
-    @projects_near_me = Project.approved.near([@lat, @long], 50).limit(4).includes(:user)
+    @ip = request.remote_ip
+    @projects_near_me = Project.approved.near(@ip, 50).limit(4).includes(:user)
     if user_signed_in?
       @projects_from_friends = current_user.get_friends_projects
       @friends_projects = @projects_from_friends.first(4)
