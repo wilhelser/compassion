f0c93f02160063dce4270755f22276f910e5f510
Wil Helser <wil@wilhelser.com>
2014-01-22 13:40:28 -0600

    post to compassion on action create

--- app/models/project.rb
+++ app/models/project.rb
@@ -29,7 +29,7 @@ class Project < ActiveRecord::Base
   accepts_nested_attributes_for :contributions
   before_save :set_key, :if => :featured_image_changed?
   before_save :set_video_key, :if => lambda { self.featured_video.present? }
-  # after_create :post_to_compassion
+  after_create :post_to_compassion
   after_create :send_new_project_email
   after_create :notify_empty_contractors
 
