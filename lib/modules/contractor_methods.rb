e333413e93656de3411072b7633ccedc66eb705b
Wil Helser <wil@wilhelser.com>
2014-01-23 21:17:30 -0600

    updates to adjuster find methods

--- lib/modules/contractor_methods.rb
+++ lib/modules/contractor_methods.rb
@@ -1,14 +1,14 @@
 module ContractorMethods
 
   def find_closest_adjuster(project)
-    @project = self.project
-    @adjuster_within_fifty = Adjuster.near(@project, 50).pluck(:id).to_a
-    @adjuster_within_hundred = Adjuster.near(@project, 100).pluck(:id).to_a
+    @project = project
+    @adjuster_within_fifty = Adjuster.near(@project, 50)
+    @adjuster_within_hundred = Adjuster.near(@project, 100)
     if @adjuster_within_fifty.any?
-      @adjuster = @adjuster_within_fifty.first
+      @adjuster = @adjuster_within_fifty.shuffle.each.first
     elsif @adjuster_within_hundred.any?
       send_no_adjuster_mailer(@project)
-      @adjuster = @adjuster_within_hundred.first
+      @adjuster = @adjuster_within_hundred.shuffle.each.first
     else
       send_no_adjuster_mailer(@project)
       @adjuster = nil
@@ -23,7 +23,7 @@ module ContractorMethods
   end
 
   def create_assignment(project, adjuster)
-    Assignment.create(:project_id => project.id, :adjuster_id => adjuster)
+    Assignment.create(:project_id => project.id, :adjuster_id => adjuster.id)
   end
 
 end
