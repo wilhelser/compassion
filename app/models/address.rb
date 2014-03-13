b57a9db3aa0f4cda2458638d552bf4e7c98346e3
Wil Helser <wil@wilhelser.com>
2014-03-13 16:39:46 -0500

    removed unused tables

--- app/models/address.rb
+++
@@ -1,4 +0,0 @@
-class Address < ActiveRecord::Base
-  attr_accessible :addres2, :address, :address_type, :city, :state, :zip_code
-  belongs_to :property
-end
--- app/models/reference.rb
+++
@@ -1,5 +0,0 @@
-class Reference < ActiveRecord::Base
-  attr_accessible :address, :address2, :city, :company_name, :contact_first_name, :contact_last_name, :contractor_id, :email, :phone_number, :reference_type, :state, :time_affiliated, :zip_code
-
-  belongs_to :contractor, :dependent => :destroy
-end
