c2f766735f677da997f6b1c3ad823bef03e1f293
Wil Helser <wil@wilhelser.com>
2014-03-21 18:00:51 -0500

    set cookies for beta access

--- app/controllers/invite_codes_controller.rb
+++ app/controllers/invite_codes_controller.rb
@@ -5,7 +5,13 @@ class InviteCodesController < ApplicationController
     elsif params[:access_code] != 'create!907'
       redirect_to :root, flash: { notice: "Invalid invite code."}
     else
+      set_tracking_cookie
       redirect_to new_user_registration_path
     end
+  end
+
+  private
+  def set_tracking_cookie
+    cookies.permanent[:beta_access] = true unless cookies[:beta_access]
   end
 end
