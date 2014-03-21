a9959789265fdb78468423526bed375817b97f5b
Wil Helser <wil@wilhelser.com>
2014-03-21 17:39:58 -0500

    updates to beta invites

--- app/controllers/invite_codes_controller.rb
+++ app/controllers/invite_codes_controller.rb
@@ -1,9 +1,9 @@
 class InviteCodesController < ApplicationController
   def validate
     if params[:access_code].blank?
-      redirect_to :root
-    elsif params[:access_code] != 'balls'
-      redirect_to :root
+      redirect_to :root, flash: { notice: "Invalid invite code."}
+    elsif params[:access_code] != 'create!907'
+      redirect_to :root, flash: { notice: "Invalid invite code."}
     else
       redirect_to new_user_registration_path
     end
