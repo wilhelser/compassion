7d08a75d14ac2f1c098a0153835767b4f3ce6321
Wil Helser <wil@wilhelser.com>
2014-05-12 17:40:56 -0500

    new beta mailchimp list id

--- lib/modules/beta_request_methods.rb
+++ lib/modules/beta_request_methods.rb
@@ -4,6 +4,6 @@ module BetaRequestMethods
     @first_name = @request.name.split(' ')[0]
     @last_name = @request.name.split(' ')[1] ||= ''
     gb = Gibbon::API.new
-    gb.lists.subscribe({:id => 'e8104c8ad0', :email => {:email => @request.email}, :merge_vars => {:FNAME => @first_name, :LNAME => @last_name}, :double_optin => false})
+    gb.lists.subscribe({:id => '2f91b1c3eb', :email => {:email => @request.email}, :merge_vars => {:FNAME => @first_name, :LNAME => @last_name}, :double_optin => false})
   end
 end
